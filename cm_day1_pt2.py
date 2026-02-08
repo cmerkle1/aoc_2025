022793ba74d3d8ece82aabcd94f7f6f7f6d60363
cmerkle1 <119691859+cmerkle1@users.noreply.github.com>
2026-02-08 15:40:13 -0500

    Create cm_day1_pt2.py

---
+++ cm_day1_pt2.py
@@ -0,0 +1,32 @@
+file_path = 'sample.txt'
+
+with open(file_path, 'r') as file:
+    num_list = [
+        int(line.rstrip('\n').replace('R', '').replace('L', '-')) for line in file
+        ]
+
+# Dial still starts at 50
+position = 50
+
+# Counter keeps track of passes over 0
+zero_counter = 0
+
+for num in num_list:
+    if num > 0:
+        step = 1  # Moving right
+    else:
+        step = -1  # Moving left
+
+    # Number of times the dial moves to get to the new number
+    movements = abs(num)
+
+    # Check position each time for the number of movements to see if 0 is passed
+    for i in range(movements):
+        # Wraps around dial accounting for stopping point at 99
+        position = (position + step) % 100
+
+        # If dial points to 0, add to counter
+        if position == 0:
+            zero_counter += 1
+
+print(zero_counter)
